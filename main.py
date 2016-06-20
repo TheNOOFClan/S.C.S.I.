f6681d7b021a1df64274020b390ed1208a62d9ae
onionsans <artem.astapchuk@gmail.com>
2016-06-19 22:55:52 -0700

    Your inconsistent indentation and use of tab characters bothers me. Stop it.

--- main.py
+++ main.py
@@ -27,24 +27,24 @@ logger.info("Starting SCSI {0} using discord.py {1}".format(ds['bot']["version"]
 print("Starting SCSI {0} using discord.py {1}".format(ds['bot']['version'], discord.__version__))
 
 def findChannel(name):
-	channels = list(bot.get_all_channels())
-	for all in channels:
-		if all.name == name:
-			return all
-		else:
-			return -1
+    channels = list(bot.get_all_channels())
+    for all in channels:
+        if all.name == name:
+            return all
+        else:
+            return -1
 
 def checkRole(user, roleRec):
-        ok = False
-        for all in list(user.roles):
-                if all.name == roleRec:
-                        ok = True
-        return ok
+    ok = False
+    for all in list(user.roles):
+        if all.name == roleRec:
+            ok = True
+    return ok
 
 @bot.event
 async def on_channel_delete(channel):
-	msg = "Channel {0} has been deleted!".format(channel.mention)
-	await bot.send_message(findChannel(ds['server']['announcements']), msg, tts=ds['bot']['tts'])
+    msg = "Channel {0} has been deleted!".format(channel.mention)
+    await bot.send_message(findChannel(ds['server']['announcements']), msg, tts=ds['bot']['tts'])
 
 @bot.event
 async def on_channel_create(channel):
@@ -58,17 +58,17 @@ async def on_member_join(member):
 
 @bot.event
 async def on_member_remove(member):
-	msg = "Member {0} has left the server!".format(member.name)
-	await bot.send_message(findChannel(ds['server']['announcements']), msg, tts=ds['bot']['tts'])
+    msg = "Member {0} has left the server!".format(member.name)
+    await bot.send_message(findChannel(ds['server']['announcements']), msg, tts=ds['bot']['tts'])
 
 @bot.event
 async def on_command(command, ctx):
-	message = ctx.message
-	destination = None
-	if message.channel.is_private:
-		destination = "Private Message"
-	else:
-		destination = "#{0.channel.name} ({0.server.name})".format(message)
+    message = ctx.message
+    destination = None
+    if message.channel.is_private:
+        destination = "Private Message"
+    else:
+        destination = "#{0.channel.name} ({0.server.name})".format(message)
 
 @bot.command()
 async def test():
@@ -77,60 +77,62 @@ async def test():
 
 @bot.command(pass_context=True)
 async def shutdown(ctx):
-        '''Shuts down the bot'''
-        author = ctx.message.author
-        if checkRole(author, ds['bot']['botmin']):
-                msg = "Shutting down now!"
-                await bot.say(msg)
-                bot.logout()
-                settings.close()
-                sys.exit()
-        else:
-                await bot.say("User is not {0}, ask a {0} to use this command!".format(ds['bot']['botmin']))
+    '''Shuts down the bot'''
+    author = ctx.message.author
+    if checkRole(author, ds['bot']['botmin']):
+        msg = "Shutting down now!"
+        await bot.say(msg)
+        bot.logout()
+        settings.close()
+        sys.exit()
+    else:
+        await bot.say("User is not {0}, ask a {0} to use this command!".format(ds['bot']['botmin']))
         
 @bot.command()
 async def pls():
-        await bot.say("I am sorry, I can not be better")
+    '''Exists'''
+    await bot.say("I am sorry, I can not be better")
 
 
 @bot.command()
 async def timeup():
-        '''Displays time up'''
-        timeUp = time.time() - startTime
-        hoursUp = timeUp // 36000
-        timeUp %= 36000
-        minutesUp = timeUp // 60
-        timeUp = round(timeUp % 60, 0)
-        msg = "Time up is: *{0} Hours, {1} Minutes and, {2} Seconds*".format(hoursUp, minutesUp, timeUp)
-        await bot.say(msg)
+    '''Displays time up'''
+    timeUp = time.time() - startTime
+    hoursUp = timeUp // 36000
+    timeUp %= 36000
+    minutesUp = timeUp // 60
+    timeUp = round(timeUp % 60, 0)
+    msg = "Time up is: *{0} Hours, {1} Minutes and, {2} Seconds*".format(hoursUp, minutesUp, timeUp)
+    await bot.say(msg)
         
 #the following code does not work but we will keep it
 @bot.command(pass_context=True)
 async def tts(ctx):
-        '''Turns TTS on or off'''
-        if ctx.message.content[len(prefix) + 4:] == "on":
-                ds['bot']['tts'] = True
-                await bot.say("TTS is now on!")
-        elif ctx.message.content[len(prefix) + 4:] == "off":
-                ds['bot']['tts'] = False
-                await bot.say("TTS is now off!")
+    '''Turns TTS on or off'''
+    if ctx.message.content[len(prefix) + 4:] == "on":
+        ds['bot']['tts'] = True
+        await bot.say("TTS is now on!")
+    elif ctx.message.content[len(prefix) + 4:] == "off":
+        ds['bot']['tts'] = False
+        await bot.say("TTS is now off!")
 
 @bot.command(pass_context=True)
 async def echo(ctx):
-        '''Echos a message'''
-        print('Echoing: ', ctx.message.content[len(prefix): + 5])
-        logger.info('Echoing: {0}'.format(ctx.message.content[len(prefix) + 5:]))
-        await bot.say(ctx.message.content[len(prefix) + 5:])
+    '''Echos a message'''
+    print('Echoing: ', ctx.message.content[len(prefix): + 5])
+    logger.info('Echoing: {0}'.format(ctx.message.content[len(prefix) + 5:]))
+    await bot.say(ctx.message.content[len(prefix) + 5:])
 
 @bot.command(pass_context=True)
 async def changegame(ctx):
-        author = ctx.message.author
-        if checkRole(author, ds['bot']['botmin']):
-                gameName = ctx.message.content[len(prefix) + 10:]
-                await bot.change_status(game=discord.Game(name=gameName))
-                await bot.say("Changing game to: \"{0}\"!".format(gameName))
-        else:
-                await bot.say("User is not {0}, ask a {0} to use this command!".format(ds['bot']['botmin']))
+    '''Changes the game being displayed'''
+    author = ctx.message.author
+    if checkRole(author, ds['bot']['botmin']):
+        gameName = ctx.message.content[len(prefix) + 10:]
+        await bot.change_status(game=discord.Game(name=gameName))
+        await bot.say("Changing game to: \"{0}\"!".format(gameName))
+    else:
+        await bot.say("User is not {0}, ask a {0} to use this command!".format(ds['bot']['botmin']))
                 
 @bot.event
 async def on_ready():
